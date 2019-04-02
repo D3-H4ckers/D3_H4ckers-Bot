963b26f9ed79cf4242167f941b0bcbaaf128d293
Trenten Norwat <tnorwat@gmail.com>
2019-04-02 17:51:23 -0500

    added support for files upload

--- cogs/hello.py
+++ cogs/hello.py
@@ -1,4 +1,5 @@
 import discord
+import random
 from util import default
 from discord.ext import commands
 
@@ -22,10 +23,27 @@ class Hello(commands.Cog):
     async def join(self, ctx):
         channel = ctx.author.voice.channel
         await channel.connect(timeout=60.0, reconnect=True)
-    
+
+
     @commands.command(pass_context=True)
     async def leave(self, ctx):
         await ctx.voice_client.disconnect()
+
+    @commands.command(pass_context=True)
+    async def ping(self, ctx):
+        await ctx.send('Pong')
+
+    @commands.command(pass_context=True)
+    async def coinflip(self, ctx):
+        flip = random.randint(0,1)
+        if flip == 0:
+            await ctx.send('Heads', file=discord.File('images/heads.png'))
+        else:
+            await ctx.send('Tails', file=discord.File('images/tails.png'))
+
+    @commands.command(pass_context=True)
+    async def name(self, ctx):
+        await ctx.send(f'Your Name Is {ctx.message.author.name}!')
 
 
 def setup(bot):
