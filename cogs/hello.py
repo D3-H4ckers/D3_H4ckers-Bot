a7fffbafd573d6eb4743cd37f6077f727712c20a
Trenten Norwat <tnorwat@gmail.com>
2019-04-02 13:19:51 -0500

    added join and leave

--- cogs/hello.py
+++ cogs/hello.py
@@ -13,10 +13,19 @@ class Hello(commands.Cog):
     async def hi(self, ctx):
         await ctx.send(f'Hello {ctx.message.author.name}!')
 
-    @commands.command()
+    @commands.command(pass_context=True)
     async def about(self, ctx):
         await ctx.send(f'Name : {self.config.name}')
         await ctx.send(f'Developers : {self.config.developers}')
+
+    @commands.command(pass_context=True)
+    async def join(self, ctx):
+        channel = ctx.author.voice.channel
+        await channel.connect(timeout=60.0, reconnect=True)
+    
+    @commands.command(pass_context=True)
+    async def leave(self, ctx):
+        await ctx.voice_client.disconnect()
 
 
 def setup(bot):
