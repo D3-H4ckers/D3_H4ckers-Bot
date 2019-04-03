d56dee45bd50a1e98227ca681dfde93924ff83b5
Trenten <tnorwat@gmail.com>
2019-04-03 17:35:17 -0500

    updated limit to 1000

--- cogs/clear.py
+++ cogs/clear.py
@@ -2,7 +2,6 @@ from util import default
 import asyncio
 import discord
 from discord.ext import commands
-from discord.ext.commands import Bot
 
 
 class Clear(commands.Cog):
@@ -11,31 +10,36 @@ class Clear(commands.Cog):
         self.bot = bot
         self.config = default.getJSON('config.json')
 
-
     @commands.command(pass_context=True)
-    async def clear(self, ctx, num):
-        try:
-            num = int(num.replace(' ', ''))
+    async def clear(self, ctx, num=None):
+        if num is None:
+            await ctx.send(f'The syntax for **clear** command is: `{self.config.prefix}clear [number of messages to delete <= 1000]`')
+        else:
+            try:
+                num = int(num)
+                if num <= 1000:
+                    await ctx.message.delete()
+                    await ctx.channel.purge(limit=num)
+                    type_message = 'message' if num == 1 else 'messages'
+                    message = await ctx.send(f'Deleted `{num}` {type_message}!')
+                    await asyncio.sleep(5)
+                    await message.delete()
+                else:
+                    await ctx.send('Number of messages to delete must be <= 1000')
 
-            if isinstance(num, int):
-                await ctx.message.delete()
-                await ctx.channel.purge(limit=num)
-
-                message = await ctx.send(f'Deleted `{num}` messages!')
+            except discord.Forbidden:
+                message = await ctx.send("You don't have permission to execute this command.")
                 await asyncio.sleep(5)
                 await message.delete()
-            else:
-                await ctx.send(f'The syntax for **clear** command is: `{self.config.prefix}clear [number of messages to delete]`')
 
-        except discord.Forbidden:
-            message = await ctx.send("You don't have permission to execute this command.")
-            await asyncio.sleep(5)
-            await message.delete()
+            except discord.HTTPException:
+                message = await ctx.send("Deleting messages failed.")
+                await asyncio.sleep(5)
+                await message.delete()
 
-        except discord.HTTPException:
-            message = await ctx.send("Deleting messages failed.")
-            await asyncio.sleep(5)
-            await message.delete()
+            except ValueError:
+                await ctx.send(f'That is not a number! `{self.config.prefix}clear [number of messages to delete <= 1000]`')
+
 
 def setup(bot):
     bot.add_cog(Clear(bot))
